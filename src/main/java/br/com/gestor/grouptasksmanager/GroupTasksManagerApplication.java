029a72bd1ef4ad0906053fc3131947ac389e6919
Gustavo Junkes <gujunkes50@gmail.com>
2022-04-12 10:25:45 -0300

    Adicionando arquivos do projeto.

---
+++ src/main/java/br/com/gestor/grouptasksmanager/GroupTasksManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestor.grouptasksmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupTasksManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupTasksManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/gestor/grouptasksmanager/GroupTasksManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gestor.grouptasksmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupTasksManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
